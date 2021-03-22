b25db266c55c6022842f6234156e97d0c5dece2d
Yurong Zhao <yurong.zhao@lianatech.com>
2021-03-22 22:39:25 +0200

    remove comments

--- src/routes/discovery.ts
+++ src/routes/discovery.ts
@@ -12,38 +12,4 @@ router.get('/', (req, res) => {
 	res.send(discoveryService.getDiscoveries(lng, lat));
 });
 
-// router.get('/:id', (req, res) => {
-// 	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-// 	res.send(patientService.getPatientById(req.params.id));
-// });
-
-// router.post('/', (req, res) => {
-// 	console.log(req.body);
-// 	try {
-// 		const newPatientEntry = entry.toNewPatientEntry(req.body);
-
-// 		const addedPatient = patientService.addPatient(newPatientEntry);
-// 		res.json(addedPatient);
-// 	} catch (e) {
-// 		res.status(400).send(e.message);
-// 	}
-// });
-
-// router.get('/:id/entries', (req, res) => {
-// 	const patient = patientService.getPatientById(req.params.id);
-// 	res.send(patient?.entries);
-// });
-
-// router.post('/:id/entries', (req, res) => {
-// 	try {
-// 		const newEntry = entry.toNewEntry(req.body);
-// 		const patient = patientService.getPatientById(req.params.id);
-// 		const entryData = patient? patient.entries: [];
-// 		const addedEntry = patientService.addEntry(newEntry, entryData);
-// 		res.json(addedEntry);
-// 	} catch (e) {
-// 		res.status(400).send(e.message);
-// 	}
-// });
-
 export default router;
